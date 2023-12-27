6206a154665b83b6567a9b8aab1dc8ef91a81576
Varun Doshi <doshivarun202@gmail.com>
2023-12-27 12:08:03 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ async fn main()->std::io::Result<()>{
         App::new().
         service(index).
         service(lookup::gas_lookup)
-    }).bind(("127.0.0.1",8080))?.
+    }).bind(("0.0.0.0",8080))?.
     run().
     await;
 
